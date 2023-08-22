4139a60aa19821cec43a2c4342b35304c364328a
Pedro Henrique <phd19994@gmail.com>
2023-08-22 00:09:59 -0300

    WTR-0001/project-routing-test-frontend

---
+++ wtr_frontend_project/app/page.tsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      Work Time Record Project
+    </main>
+  )
+}
---
+++ wtr_frontend_project/app/test/page.tsx
@@ -0,0 +1,5 @@
+import TestComponent from "../components/testComponent";
+
+export default function Page() {
+    return <TestComponent></TestComponent>
+}
