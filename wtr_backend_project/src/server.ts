cfb95d9ac0ae2189b53da5b42075cded2d2147c4
Anderson <loyolaandersom2013@gmail.com>
2023-08-24 20:14:11 -0300

    feat: adds working bootstrap route

--- wtr_backend_project/src/server.ts
+++ wtr_backend_project/src/server.ts
@@ -1,15 +1,4 @@
-const express = require('express');
-const cors = require('cors')
-
-const routes = require('./routes')
-
-
-const app = express();
-
-app.use(cors())
-app.use(express.json());
-
-app.use('/', routes);
+import { app } from "./app";
 
 app.listen(8800, ()=>{
     console.log("server is running on port 8800")
