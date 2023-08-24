3200a75df64d380b49b805922b64682b27f5e2f9
Anderson <loyolaandersom2013@gmail.com>
2023-08-24 20:10:33 -0300

    feat: removes bootstrap files

--- wtr_backend_project/src/routes/index.routes.ts
+++
@@ -1,8 +0,0 @@
-import { ProjectRoutesWrapper } from './projects.routes'
-import { UserRoutesWrapper } from './users.routes'
-
-export const init =  (controllers) => ({
-    projectRouter: new ProjectRouterWrapper(controllers.projectController),
-    userRouter: new UserRouterWrapper(controllers.userController)
-    timeRecordsRouter: new TimeRecordsWrapper(controllers.timeRecordsController),
-})
