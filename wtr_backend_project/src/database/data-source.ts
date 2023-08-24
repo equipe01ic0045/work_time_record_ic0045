b9d8de581931da09ca750e534a097cc5c3cc94d0
Anderson <loyolaandersom2013@gmail.com>
2023-08-24 17:30:42 -0300

    feat: datasource and migration files

--- wtr_backend_project/src/database/data-source.ts
+++ wtr_backend_project/src/database/data-source.ts
@@ -1,12 +1,24 @@
-import { DataSource } from 'typeorm';
-import { config } from '../config';
+import { DataSource, DataSourceOptions } from "typeorm";
+import dotenv from "dotenv";
+import { Users1692901867852 } from './migrations/1692901867852-Users';
+import { Projects1692901890203  } from './migrations/1692901890203-Projects';
+import { TimeRecords1692901898968 } from './migrations/1692901898968-Time-records';
 
-const postgresDataSource = new DataSource({
+dotenv.config();
+
+const config: DataSourceOptions = {
     type: 'postgres',
-    host: config.database.host,
-    port: config.database.port,
-    username: config.database.username,
-    password: config.database.password,
-});
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    entities: [],
+    migrations: [
+        Users1692901867852,
+        Projects1692901890203,
+        TimeRecords1692901898968,
+    ],
+}
 
-export default postgresDataSource;
+export default new DataSource(config);
