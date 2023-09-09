2e7da59d5f6dbf22c8d86720b02972af62d8ef64
Pedro Henrique <phd19994@gmail.com>
2023-09-08 21:04:04 -0300

    feature:added-configuration-routing

---
+++ wtr_frontend_project/src/app/page.tsx
@@ -0,0 +1,5 @@
+// page ui generated from the url
+
+export default function Page() {
+    return <h1>Hello, Home page!</h1>
+  }
--- wtr_frontend_project/src/components/side-menu-component/index.tsx
+++ wtr_frontend_project/src/components/side-menu-component/index.tsx
@@ -1,22 +1,43 @@
+'use client';
 import { Box, Button } from "@chakra-ui/react";
+import Link from "next/link";
 import React from "react";
 
 export default function SideMenuComponent() {
-    const pages = ["projects", "timeRecords", "justifications"]
+    const pages = [
+        {
+            id: 1,
+            name: "Projects",
+            link: "projects"
+        }, {
+            id: 2,
+            name: "Time Records",
+            link: "time-records"
+        }, {
+            id: 3,
+            name: "Justifications",
+            link: "justifications"
+        }
+    ]
     return (
         <Box
             display={"flex"}
             flexDirection={"column"}
             alignItems={"center"}
-            justifyContent={"center"}
+            justifyContent={"start"}
+            gap={"0.5em"}
             bg={"blueviolet"}
+            padding={"0.5em"}
+            width={"20%"}
         >
             {pages.map((item) => {
                 return <Button
-                bg={"white"}
-                borderRadius={"5px"}
-                padding={"0.5em"}
-                >{item}</Button>
+                    bg={"white"}
+                    borderRadius={"5px"}
+                    padding={"1em"}
+                    width={"100px"}
+                    key={item.id}
+                ><Link href={`/${item.link}`}>{item.name}</Link></Button>
             })}
         </Box>
     );
