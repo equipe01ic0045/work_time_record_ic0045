0ea61c2dfe6a1e215abbc7579d32eb2c191bd25e
Anderson <loyolaandersom2013@gmail.com>
2023-09-24 13:07:35 -0300

    chore: removes domain layer

--- wtr_backend_project/src/domain/entities/ProjectConfiguration.ts
+++
@@ -1,9 +0,0 @@
-import { Project } from "./Project";
-
-export class ProjectConfiguration {
-    project_documents: boolean;
-    project_time_interval: Date;
-    project_time_start: Date;
-    project_time_end: Date;
-    project?: ProjectConfiguration; 
-}
--- wtr_backend_project/src/domain/repositories/ProjectConfigurationRepository.ts
+++
@@ -1,8 +0,0 @@
-
-import { ProjectConfiguration } from "../entities/ProjectConfiguration";
-
-export interface ProjectConfigurationRepository {
-    findByProjectId(projectId: number): Promise<ProjectConfiguration | null>;
-    create(configuration: ProjectConfiguration): Promise<ProjectConfiguration>;
-    update(configuration: ProjectConfiguration): Promise<ProjectConfiguration>;
-}
