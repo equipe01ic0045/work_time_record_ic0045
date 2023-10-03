c42fa35b6015292a0c4c2a88cbaf62702146c913
Thiago Coutinho Sousa Silva <tcoutinhossilva@gmail.com>
2023-10-02 22:15:02 -0300

    feature:tela-de-cadastro

--- wtr_frontend_project/src/app/auth/page.tsx
+++ wtr_frontend_project/src/app/auth/page.tsx
@@ -42,103 +42,51 @@ export default function LoginComponent() {
   }
 
   return (
-    <Box
-      display={"flex"}
-      flexDirection={"column"}
-      alignItems={"center"}
-      justifyContent={"start"}
-      gap={"0.5em"}
-      padding={"0.5em"}
-      width={"100%"}
-    >
-      <Text 
-        fontSize={"6xl"} 
-        color={"blueviolet"} 
-        //fontWeight={'bold'}
-      >
-        Ponto Certo
-      </Text>
-      <Box display={"flex"} flexDirection={"column"}>
-        <Box alignItems={"flex-start"}> 
-          <Text 
-            fontSize={"2xl"} 
-            color={"blueviolet"}
-          >
-              Entrar
-          </Text>
+    <Box display="flex" flexDirection="column" alignItems="center" gap="0.5em" p="0.5em" w="100%">
+      <Text fontSize="6xl" color="blueviolet">Ponto Certo</Text>
+      <Box display="flex" flexDirection="column">
+        <Box alignItems="flex-start">
+          <Text fontSize="2xl" color="blueviolet">Entrar</Text>
         </Box>
-        <Box mt={"0.5em"}>
-          <Input
-            placeholder="email"
-            type="email"
-            name="email"
-            value={user.email}
-            onChange={inputHandler}
-            style={{
-              color: 'blueviolet', 
-              backgroundColor: 'Lavender'
-            }}
-          />
-        </Box>
-        <Box position="relative" mt={"1em"}>
+        <Input
+          placeholder="email"
+          type="email"
+          name="email"
+          value={user.email}
+          onChange={inputHandler}
+          bgColor="Lavender"
+          color="blueviolet"
+          mt="1em"
+        />
+        <Box position="relative" mt="1em">
           <Input
             placeholder="senha"
-            type={showPassword ? 'text' : 'password'} 
+            type={showPassword ? 'text' : 'password'}
             name="password"
             value={user.password}
             onChange={inputHandler}
-            style={{
-              color: 'blueviolet', 
-              backgroundColor: 'Lavender',
-              paddingRight: '40px', 
-            }}
+            bgColor="Lavender"
+            color="blueviolet"
+            paddingRight="40px"
           />
-          <Box 
-            position="absolute" 
-            right="10px" 
-            top="50%" 
-            transform="translateY(-50%)"
-            cursor="pointer"
-            onClick={() => setShowPassword(!showPassword)}
-          >
+          <Box position="absolute" right="10px" top="50%" transform="translateY(-50%)" cursor="pointer" onClick={() => setShowPassword(!showPassword)}>
             {showPassword ? <ViewOffIcon /> : <ViewIcon />}
           </Box>
         </Box>
-        <Box display="flex" justifyContent="flex-end" alignItems="flex-end">
-            <Link href="">
-                <Text fontSize="sm">
-                    Perdeu a senha ?
-                </Text>
-            </Link>
+        <Box display="flex" justifyContent="flex-end" alignItems="flex-end" mt="1em">
+          <Link href=""><Text fontSize="sm">Perdeu a senha?</Text></Link>
         </Box>
-        <Box mt={"1em"}>
-          <Button
-            background={"blueviolet"}
-            textColor={"white"}
-            onClick={loginHandler}
-            w="100%"
-          >
-            login
-          </Button>
-        </Box >
-        <Box display={"flex"} flexDirection="column" gap={"0.5em"} alignItems="center" justifyContent="center" mt={"1em"}>
-          <Box display={"flex"} flexDirection={"row"} gap={"1em"}>
-            <Text fontSize="sm">Não é registrado ?</Text>
-            <Link href={"/auth/register"}>
-              <Text fontSize="sm" color={"blue"}>registre-se</Text>
-            </Link>
+        <Button bg="blueviolet" color="white" onClick={loginHandler} w="100%" mt="1em">login</Button>
+        <Box display="flex" flexDirection="column" gap="0.5em" alignItems="center" mt="1em">
+          <Box display="flex" flexDirection="row" gap="1em">
+            <Text fontSize="sm">Não é registrado?</Text>
+            <Link href="/auth/register"><Text fontSize="sm" color="blue">registre-se</Text></Link>
           </Box>
           <Text fontSize="sm">Ou continue com:</Text>
-          <Box display={"flex"} flexDirection={"row"}>
-            <Link href="/auth/google">
-              <Box as={FaGoogle} size="24px" cursor="pointer" aria-label="Google Login" />
-            </Link>
-            <Link href="/auth/facebook">
-              <Box as={FaFacebookF} size="24px" cursor="pointer" aria-label="Facebook Login" />
-            </Link>
-            <Link href="/auth/github">
-              <Box as={FaGithub} size="24px" cursor="pointer" aria-label="GitHub Login" />
-            </Link>
+          <Box display="flex" flexDirection="row" gap="1em">
+            <Link href="/auth/google"><Box as={FaGoogle} size="24px" cursor="pointer" aria-label="Google Login" /></Link>
+            <Link href="/auth/facebook"><Box as={FaFacebookF} size="24px" cursor="pointer" aria-label="Facebook Login" /></Link>
+            <Link href="/auth/github"><Box as={FaGithub} size="24px" cursor="pointer" aria-label="GitHub Login" /></Link>
           </Box>
         </Box>
       </Box>
