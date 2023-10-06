4d27eb15c435b617a741e1125aa6d4d40b8e9cb0
Valdinei Ferreira <valdineifer.dev@outlook.com>
2023-10-05 22:46:13 -0300

    chore(time-records): remove documents list and make adjustments
    - add single attachment fpr dcument
    - delete old page for time record info
    - adjust form for justifications

--- wtr_frontend_project/src/app/main/time-records/project/[projectId]/info/[timeRecordId]/justify/page.tsx
+++ wtr_frontend_project/src/app/main/time-records/project/[projectId]/info/[timeRecordId]/justify/page.tsx
@@ -4,37 +4,17 @@ import HeaderBox from "@/components/global/HeaderBox";
 import DocumentRow from "@/components/time-records/documentRow";
 import RecordDocument from "@/types/RecordDocument";
 import { AddIcon, EditIcon } from "@chakra-ui/icons";
-import { Box, Button, Card, CardBody, CardHeader, HStack, Heading, Icon, IconButton, Text, Textarea, useToast } from "@chakra-ui/react";
+import { Box, Button, ButtonGroup, Card, CardBody, CardFooter, CardHeader, Divider, FormControl, FormHelperText, FormLabel, HStack, Heading, Icon, IconButton, Input, InputGroup, InputRightAddon, Stack, StackDivider, Text, Textarea, useToast } from "@chakra-ui/react";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 export default function Page({ params }: { params: { timeRecordId: string } }) {
   const [description, setDescription] = useState<string>('');
-  const [documents, setDocuments] = useState<RecordDocument[]>([]);
+  const [document, setDocument] = useState<File | null>();
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
 
   const router = useRouter();
   const toast = useToast();
-
-  function onSelectFileOnIndex(index: number) {
-    return (data: RecordDocument) => {
-      documents[index] = data;
-      setDocuments(
-        documents.map((doc, i) => i === index ? data : doc)
-      );
-    }
-  }
-
-  function removeDocumentRow(index: number) {
-    return () => {
-      const cloned = [...documents];
-      cloned.splice(index, 1);
-      setDocuments(cloned);
-    }
-  }
-
-  function addDocument() {
-    setDocuments([...documents, { file: null, description: '' }]);
-  }
 
   async function handleSubmit(): Promise<void> {
     toast({
@@ -52,43 +32,50 @@ export default function Page({ params }: { params: { timeRecordId: string } }) {
     <Box w="100%">
       <HeaderBox title={`Registro de ponto ${params.timeRecordId}`} />
       <Box mx="auto" mt={20} w="40%">
-        <HStack mb={10} gap={3}>
-          <EditIcon boxSize="3rem" />
-          <Heading as="h2" size="xl">
-            Envio de justificativa
-          </Heading>
-        </HStack>
 
-        <Card marginY={5} variant={"filled"} w="100%">
-          <CardHeader display="flex" justifyContent={"space-between"}>
-            <Heading as="h2" size={"lg"}>Lista de Documentos</Heading>
-            <IconButton bg={"white"} aria-label="Adicionar" icon={<AddIcon />} onClick={addDocument} />
-          </CardHeader>
-          <CardBody>
-            {documents.map((row, index) => (
-              <DocumentRow
-                description={row.description}
-                file={row.file}
-                handleRemove={removeDocumentRow(index)}
-                key={index}
-                onInputChange={onSelectFileOnIndex(index)} />
-            ))}
-          </CardBody>
-        </Card>
-
-        <Card marginY={5} variant={"filled"} w="100%">
+        <Card marginY={5} variant={"filled"} size="lg">
           <CardHeader>
-            <Heading aria-label="description" as="h2" size={"lg"}>Descrição</Heading>
+            <HStack gap={3}>
+              <EditIcon boxSize="3rem" />
+              <Heading as="h2" size="lg">
+                Envio de justificativa
+              </Heading>
+            </HStack>
           </CardHeader>
+
           <CardBody>
-            <Textarea
-              aria-labelledby="description"
-              variant="filled"
-              bg="white"
-              _hover={{ bg: "white" }}
-              minHeight={100}
-              value={description}
-              onChange={(e) => setDescription(e.target.value)} />
+            <Stack divider={<StackDivider />} spacing='8'>
+              <FormControl>
+                <FormLabel>Descrição</FormLabel>
+                <Textarea
+                  aria-labelledby="description"
+                  variant="filled"
+                  bg="white"
+                  _hover={{ bg: "white" }}
+                  minHeight={100}
+                  value={description}
+                  onChange={(e) => setDescription(e.target.value)}
+                />
+                <FormHelperText>Insira todos os detalhes cabíveis para sua justificativa.</FormHelperText>
+              </FormControl>
+              <FormControl>
+                <FormLabel>Selecione o arquivo</FormLabel>
+                <ButtonGroup w="100%" isAttached variant='outline' onClick={e => fileInputRef.current?.click()}>
+                  <Button w='100%'>{document?.name}</Button>
+                  <IconButton aria-label='Add to friends' icon={<AddIcon />} />
+                </ButtonGroup>
+                <Input
+                  ref={fileInputRef}
+                  type="file"
+                  hidden
+                  onChange={e => setDocument(e.target.files && e.target.files[0])}
+                />
+                <FormHelperText>
+                  Se necessário mais de um arquivo/documento,
+                  utilize ferramentas para junção destes arquivos em um só.
+                </FormHelperText>
+              </FormControl>
+            </Stack>
           </CardBody>
         </Card>
 
