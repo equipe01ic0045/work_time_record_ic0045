60399a4fb4139815939b0caf662aa9b3fa772a9f
Enzo Magalhaes <magalhaes.enzo82@gmail.com>
2023-11-12 23:37:04 -0300

    updating timeRecordData.tsx

--- wtr_frontend_project/src/types/TimeRecordData.ts
+++ wtr_frontend_project/src/types/TimeRecordData.ts
@@ -1,35 +1,5 @@
-// export type Justification = {
-//   description?: string;
-//   document?: File|null;
-//   date?: Date;
-// };
-
-// export type JustificationData = Justification & {
-//   date: Date;
-//   projectId: number;
-//   timeRecordId: number;
-// }
-
-// type TimeRecordData = Justification & {
-//   date: Date;
-//   projectId: number;
-//   location?: string;
-// };
-
-// export default TimeRecordData;
-
-
-
-// export type SimpleCheckInData = TimeRecordData & {
-//   location?: string;
-//   check_in_timestamp: Date;
-// };
-
-// export type SimpleCheckOutData = TimeRecordData & {
-//   check_out_timestamp: Date;
-// };
-
 export type SimpleTimeRecordData = {
+  project_id: number;
   timestamp: Date;
   location?: string;
 };
